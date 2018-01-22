c5d5425d0492783b54724870f84b033e8ec1f9cc
R3oLoN <leandroreolon@gmail.com>
2018-01-22 14:21:46 -0200

    Add editorconfig

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 const a = 3;
 
-
 const b = 2;
 // const b = 4
 
-const c = 'hjsgfhjgsdsjgfhsfjdkhfsdfiusydfuisydfiusydfuisydfiusydfiusydfiuysdiufasdasdasdadsasdasdasdasdasdysuidfyiusdyfuisydfuisyfdiusydfiuysiudfyuisyfisudfyuisydfi';
-
+const c =
+  'hjsgfhjgsdsjgfhsfjdkhfsdfiusydfuisydfiusydfuisydfiusydfiusydfiuysdiufasdasdasdadsasdasdasdasdasdysuidfyiusdyfuisydfuisyfdiusydfiuysiudfyuisyfisudfyuisydfi';
 
 function sum() {
   return a + b + c;
